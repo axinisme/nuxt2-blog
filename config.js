1a21249922cbc73bf951100f5704ba9ddbe82d20
axinisme <89328634+axinisme@users.noreply.github.com>
2022-08-03 20:18:29 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 export default {
-  title: 'yun yu yuan', // 网站标题
-  nickName: "云与原", // 昵称
-  githubName: 'yunyuyuan', // github账户名
+  title: 'star bulingbuling', // 网站标题
+  nickName: "星星亮了", // 昵称
+  githubName: 'axinisme', // github账户名
   githubRepo: 'nuxt2-blog', // 勿修改
   githubBranch: 'master', // 勿修改
   domain: 'https://blog.halberd.cn', // rss域名
-  SEO_description: '云与原的博客，书写值得书写的东西',
-  SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,云与原,云与原的博客',
+  SEO_description: '阿昕的博客，书写值得书写的东西',
+  SEO_keywords: 'xin,ic',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc", // cloudflare的webanalyze
 }
